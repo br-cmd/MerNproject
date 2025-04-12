c5f00e5b4972da20fe8a12172d8eb7335a953e3a
Brian Okungu <92260552+br-cmd@users.noreply.github.com>
2025-04-12 14:12:47 +0300

    Update index.js

--- GitFile/client/src/http/index.js
+++ GitFile/client/src/http/index.js
@@ -1,3 +1,4 @@
+// ======= CLIENT SIDE (api.js) =======
 import axios from "axios";
 
 const api = axios.create({
@@ -6,43 +7,43 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    
   },
 });
 
 // Auth API's
-export const login = (data) => api.post("/api/v1/auth/login", data);
-export const register = (data) => api.post("/api/v1/auth/register", data);
-export const getUser = () => api.get("/api/v1/user/get-user");
-export const logout = () => api.get("/api/v1/auth/logout");
+export const login = (data) => api.post("/auth/login", data);
+export const register = (data) => api.post("/auth/register", data);
+export const getUser = () => api.get("/user/get-user");
+export const logout = () => api.get("/auth/logout");
 
-//Job API's
-export const getJobs = () => api.get("/api/v1/job/get-jobs");
-export const addJob = (data) => api.post("/api/v1/job/create-job", data);
-export const deleteJob = (data) => api.delete(`/api/v1/job//delete-job/${data}`);
-export const filterJobs = (params) => api.get( `/api/v1/job/get-jobs?page=${params.page}&status=${params.status}&workType=${params.workType}&sort=${params.sort}&search=${params.search}`);
+// Job API's
+export const getJobs = () => api.get("/job/get-jobs");
+export const addJob = (data) => api.post("/job/create-job", data);
+export const deleteJob = (data) => api.delete(`/job/delete-job/${data}`);
+export const filterJobs = (params) =>
+  api.get(
+    `/job/get-jobs?page=${params.page}&status=${params.status}&workType=${params.workType}&sort=${params.sort}&search=${params.search}`
+  );
 
-
-api.interceptors.response.use((config) => {
-  return config;
-}, async (error) => {
-  const originalRequest = error.config;
-  if(error.response.status === 401 && error.config && !error.config._isRetry){
+// Response Interceptor for Refreshing Token
+api.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._isRetry) {
       originalRequest._isRetry = true;
-
       try {
-          await axios.get(`https://mer-nproject-gamma.vercel.app/api/v1/auth/refresh`, {
-              withCredentials : true
-          });
-
-          return api.request(originalRequest);
-
-      } catch (error) {
-          console.log(error.message);
+        await axios.get(
+          "https://mer-nproject-gamma.vercel.app/api/v1/auth/refresh",
+          { withCredentials: true }
+        );
+        return api.request(originalRequest);
+      } catch (e) {
+        console.log("Token refresh failed", e.message);
       }
+    }
+    throw error;
   }
-
-  throw error;
-})
+);
 
 export default api;
